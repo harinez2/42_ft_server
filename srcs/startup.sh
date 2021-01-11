d6fed05df4582a3b31cc4fc91f654a653e0f0067
harinez <66913862+harinez2@users.noreply.github.com>
2021-01-11 23:12:00 +0900

    support autoindex change by envvalue

--- srcs/startup.sh
+++ srcs/startup.sh
@@ -1,3 +1,9 @@
+if [ ! -v AUTOINDEX ]; then AUTOINDEX=on; fi
+echo "AUTOINDEX setting: $AUTOINDEX"
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default2
+sed -e "s/__AUTOINDEX__/$AUTOINDEX/" /etc/nginx/sites-available/default2 > /etc/nginx/sites-available/default
+rm /etc/nginx/sites-available/default2
+
 service php7.3-fpm start
 service nginx start
 mysqld_safe --user=mysql &
