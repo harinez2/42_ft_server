5ba8cd4bc8f9198dd2f38e603703bf8ee1d37da0
harinez <66913862+harinez2@users.noreply.github.com>
2021-01-12 12:09:22 +0900

    works fine if incorrect AUTOINDEX value is specified

--- srcs/startup.sh
+++ srcs/startup.sh
@@ -1,5 +1,6 @@
-if [ ! -v AUTOINDEX ]; then AUTOINDEX=on; fi
-echo "AUTOINDEX setting: $AUTOINDEX"
+echo "Specified AUTOINDEX value: $AUTOINDEX"
+if [ ! "$AUTOINDEX" = "on" ] && [ ! "$AUTOINDEX" = "off" ]; then AUTOINDEX=on; fi
+echo "AUTOINDEX value works as: $AUTOINDEX"
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default2
 sed -e "s/__AUTOINDEX__/$AUTOINDEX/" /etc/nginx/sites-available/default2 > /etc/nginx/sites-available/default
 rm /etc/nginx/sites-available/default2
